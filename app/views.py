83ea2f797468cfaab65b3b2c8e6271480cdebe3c
unknown <chosen.alfeche@gmail.com>
2017-08-25 01:57:32 +0800

    initial spero-flask

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from app import app
+from flask import render_template
+from flask_wtf import Form
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    return render_template('index.html')
+
+@app.route('/home.html')
+
+def home():
+    return render_template('home.html')
+
+@app.route('/grades.html')
+
+def grades():
+    return render_template('grades.html')
+
+@app.route('/questions.html')
+
+def questions():
+    return render_template('questions.html')
