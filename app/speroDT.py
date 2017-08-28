da6572823526ba73e79d4bb18301025532397c00
unknown <chosen.alfeche@gmail.com>
2017-08-28 16:24:37 +0800

    working on spero decision tree

--- app/speroDT.py
+++ app/speroDT.py
@@ -1,4 +1,7 @@
 from sklearn import tree
+from os import getcwd
+import pandas as pd
+from pprint import pprint
 
 class speroDT:
     def __init__(self):
@@ -6,3 +9,17 @@ class speroDT:
         Y = [0,1]
         clf = tree.DecisionTreeClassifier()
         clf = clf.fit(X,Y)
+
+    def loadDate(self):
+        path = getcwd() + "\\datasets\\HollandCode.xlsx"
+        data = pd.read_excel( path, sheetname=[0])
+        temp = sorted(set(data[0]['Occupational Title']))
+        occT = {}
+        i = 0
+        for x in temp:
+            occT[str(x)] = str(i)
+            i+=1
+        pprint(occT)
+
+s = speroDT()
+s.loadDate()
